3a075fa3595de9cfcad5dbd903acb39e68379e4b
SOME <sourbar2001@gamail.com>
2023-06-17 02:22:23 -0700

    task 5

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,67 +1,46 @@
 #include "sort.h"
 #include <stdio.h>
 /**
- *swap_node - swap a node for his previous one
- *@node: node
- *@list: node list
- *Return: return a pointer to a node which was enter it
+ * _swap - swaped 2 values.
+ * @array: the array for swap him values.
+ * @i: First index
+ * @j: Second index
+ * Return: Nothing
  */
-listint_t *swap_node(listint_t *node, listint_t **list)
+void _swap(int *array, int i, int j)
 {
-	listint_t *back = node->prev, *current = node;
-	/*NULL, 19, 48, 9, 71, 13, NULL*/
+	int tmp;
 
-	back->next = current->next;
-	if (current->next)
-		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
-}
-/**
- *cocktail_sort_list - this is a cocktail sort implementation
- *working on a double linked lists
- *@list: list
- */
-void cocktail_sort_list(listint_t **list)
-{
-	listint_t *node;
-	int swap_done = 1;
-
-	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
-		return;
-	node = *list;
-	while (swap_done == 1)
+	if (array[i] != array[j])
 	{
-		swap_done = 0;
-		while (node->next)
-		{
-			if (node->n > node->next->n)
-			{
-				node = swap_node(node->next, list);
-				swap_done = 1;
-				print_list(*list);
-			}
-			node = node->next;
-		}
-		if (swap_done == 0)
-			break;
-		swap_done = 0;
-		while (node->prev)
-		{
-			if (node->n < node->prev->n)
-			{
-				node = swap_node(node, list);
-				swap_done = 1;
-				print_list(*list);
-			}
-			else
-				node = node->prev;
-		}
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+}
+
+/**
+ * shell_sort - sort the list and print the changes
+ * @array: The array to sort.
+ * @size: Size of the array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t h = 0,  i, j;
+
+	if (size < 2)
+		return;
+
+	while (h <= size / 3)
+		h = h * 3 + 1;
+
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+				_swap(array, j, j - h);
+		h /= 3;
+		print_array(array, size);
 	}
 }
