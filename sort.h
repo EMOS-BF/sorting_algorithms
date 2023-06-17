5dfc643ccf4fab605dce1ea8f9a855516e31f5bc
SOME <sourbar2001@gamail.com>
2023-06-17 02:15:37 -0700

    sort.h reviews

--- sort.h
+++ sort.h
@@ -42,7 +42,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-listint_t *swap_node(listint_t *node, listint_t **list);
+/*Advanced Tasks*/
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
